7dcbaaff115ccb576580a8801599d9f88914a07f
ankushbhatheja14 <ankushbhatheja14@gmail.com>
2017-12-13 19:15:22 +0530

    user added on aws mongo

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
+var http      = require('http');
+
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://13.126.205.251:27017/CollegeData');
+mongoose.connect('mongodb://ankush:12345678@13.126.205.251:27017/CollegeData');
 
 module.exports = {
   mongoose
